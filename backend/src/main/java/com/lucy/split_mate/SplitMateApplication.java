7e4747432361aecaf3709a2fcb30da1f252315d0
lucyfinnerty <114194409+lucyfinnerty@users.noreply.github.com>
2025-04-14 20:03:31 -0500

    starter backend and frontend files

---
+++ backend/src/main/java/com/lucy/split_mate/SplitMateApplication.java
@@ -0,0 +1,13 @@
+package com.lucy.split_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitMateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lucy/split_mate/SplitMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucy.split_mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
