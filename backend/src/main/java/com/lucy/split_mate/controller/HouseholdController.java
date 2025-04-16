d0241f0e6819c15de8e16d963a9280cc0ba0ffd6
lucyfinnerty <114194409+lucyfinnerty@users.noreply.github.com>
2025-04-15 21:32:34 -0500

    added controllers - no mapping yet

---
+++ backend/src/main/java/com/lucy/split_mate/controller/HouseholdController.java
@@ -0,0 +1,10 @@
+package com.lucy.split_mate.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/household")
+@CrossOrigin
+public class HouseholdController {
+
+}
---
+++ backend/src/main/java/com/lucy/split_mate/controller/PaymentController.java
@@ -0,0 +1,10 @@
+package com.lucy.split_mate.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/payment")
+@CrossOrigin
+public class PaymentController {
+
+}
