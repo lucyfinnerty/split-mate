ab5969ac5d0ce4899454389c7d564de972d8d2ed
lucyfinnerty <114194409+lucyfinnerty@users.noreply.github.com>
2025-04-16 22:10:13 -0500

    added method in PaymentRepository to be used in method from RoommateService

--- backend/src/main/java/com/lucy/split_mate/repository/PaymentRepository.java
+++ backend/src/main/java/com/lucy/split_mate/repository/PaymentRepository.java
@@ -1,10 +1,12 @@
 package com.lucy.split_mate.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.lucy.split_mate.model.Payment;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-
+    List<Payment> findByFromIdOrToId(Long fromId, Long toId);
 }
